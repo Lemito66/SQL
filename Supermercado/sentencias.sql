494a4c2462456634da980660bc3b7c781527aee2
Emill <lemito66@gmail.com>
2023-05-17 20:31:25 -0500

    sentencias

--- Supermercado/sentencias.sql
+++ Supermercado/sentencias.sql
@@ -2,3 +2,12 @@ use SUPERMERCADO
 
 alter table prueba
 alter column nombre nvarchar(60)
+
+-- A�adir columna 
+
+alter table prueba
+add nombre nvarchar(60)
+
+-- Eliminar Prueba
+
+drop table prueba
