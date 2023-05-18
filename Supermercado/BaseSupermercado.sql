7654d7d27e5b068ac37944816a1e4a9e67acc926
Emill <lemito66@gmail.com>
2023-05-17 21:03:05 -0500

    select

---
+++ Supermercado/BaseSupermercado.sql
@@ -0,0 +1,10 @@
+select * from cliente
+select * from productos
+select * from factura
+
+
+insert into cliente(identificacion, nombre)
+values ('0201379492', 'Pepa')
+
+update cliente
+set genero = 1
